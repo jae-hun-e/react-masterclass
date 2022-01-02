d6a0fdb6052a6cf88964616d199aac3ecc8c7201
jaehun <wognskec@hanyang.ac.kr>
2022-01-02 19:19:52 +0900

    useState TS 사용법

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 // ! syled-component의 속성값 type
@@ -15,7 +16,7 @@ const Container = styled.div<ContainerProps>`
   border: 1px solid ${(props) => props.borderColor};
 `;
 
-// ! component의 속성값 type
+// ! Circle의 속성값 type
 interface CirecleProps {
   bgColor: string; // !!필수
   borderColor?: string; // !! 옵션
@@ -23,6 +24,9 @@ interface CirecleProps {
 
 // ! 속성값 JS에게 알려줌
 function Circle({ bgColor, borderColor }: CirecleProps) {
+  // const [value, setValue] = useState<number|string>(0) //! 타입 여러개로 지정해줄 때
+  const [count, setCount] = useState(0); //! 타입 안적으면 defalut값으로 TS가 알아서 추축한다.
+
   // ! 사용자가 정해준 속성값이 없으면 ?? 뒤에 default값 지정해줌
   return <Container bgColor={bgColor} borderColor={borderColor ?? "red"} />;
 }

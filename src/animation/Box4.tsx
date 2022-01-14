d1df48d64a71d701475e3a25d0a768f04b30f559
jaehun <wognskec@hanyang.ac.kr>
2022-01-14 20:55:47 +0900

    drop시 제자리로 돌아오기

--- src/animation/Box4.tsx
+++ src/animation/Box4.tsx
@@ -1,30 +1,40 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
-
-const boxVariants = {
-  hover: {
-    scale: 1.5,
-    rotateZ: 90,
-  },
-  click: {
-    scale: 1,
-    borderRadius: "100px",
-  },
-  drag: { backgroundColor: "rgb(46, 204, 113)", transition: { duration: 10 } },
-};
+import { useRef } from "react";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default function () {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
+
   return (
-    <Box drag variants={boxVariants} whileHover="hover" whileTap="click" />
+    <BigBox ref={biggerBoxRef}>
+      <Box
+        drag
+        dragSnapToOrigin
+        dragElastic={0.5}
+        dragConstraints={biggerBoxRef}
+      />
+    </BigBox>
   );
 }
 
-const Box = styled(motion.div)`
+const BigBox = styled.div`
   margin: 30px;
   width: 200px;
   height: 200px;
-  background-color: white;
+  border-radius: 20px;
+  background-color: rgba(255, 255, 255, 0.4);
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`;
+
+const Box = styled(motion.div)`
+  width: 100px;
+  height: 100px;
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 20px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
